936a0b4951f5593edf573aabf0982a5bebb3f0d4
Shifo3456 <67818676+Shifo3456@users.noreply.github.com>
2022-02-18 17:05:35 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include "cpr/cpr.h"
+#include <iostream>
+#include <string>
+
+int main() {
+  cpr::Response r = cpr::Get(cpr::Url{"http://httpbin.org/html"},
+                             cpr::Header{{"accept", "text/html"}});
+  int a = r.text.find("<h1>");
+  int b = r.text.find("</h1>");
+  std::string header = r.text.substr(a + 4, b - a - 4);
+  std::cout << header << std::endl;
+}
